78e01e74277735f0bf49f5241fef040997177c00
Benjamin Scranton <bscranton@stabilityit.net>
2025-08-07 12:19:58 -0500

    feat: use .env for testing creds
    I meant to update this before pushing originally. Oops.

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/bscranton/vsax-go
 go 1.24.5
 
 require (
+	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/net v0.33.0 // indirect
 	resty.dev/v3 v3.0.0-beta.3 // indirect
 )
--- vsax_test.go
+++ vsax_test.go
@@ -1,11 +1,15 @@
 package vsax
 
-import "testing"
+import (
+	"os"
+	"testing"
+	_ "github.com/joho/godotenv/autoload"
+)
 
 func TestGetAllDevices(t *testing.T) {
 	vc := NewClient("https://stabilityit.vsax.net",
-		"KEY",
-		"TOKEN")
+		os.Getenv("VSAX_CLIENT_KEY"),
+		os.Getenv("VSAX_CLIENT_TOKEN"))
 	ad, err := vc.GetAllDevices()
 	if err != nil {
 		t.FailNow()
@@ -13,4 +17,11 @@ func TestGetAllDevices(t *testing.T) {
 	if ad.Meta.ResponseCode != 200 {
 		t.FailNow()
 	}
+}
+
+func TestGetDevice(t *testing.T) {
+	vc := NewClient("https://stabilityit.vsax.net",
+		os.Getenv("VSAX_CLIENT_KEY"),
+		os.Getenv("VSAX_CLIENT_TOKEN"))
+	device, err := vc.GetDevice("1e6aaa00-0d65-4767-a24d-71955fc57573")
 }
